18ab8e013208481efc0577614e37cc4473af4f57
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-02-11 15:07:26 -0300

    Ajustanto o código que envia o nome do Qr-code para o Postaman

--- payments/pix.py
+++ payments/pix.py
@@ -15,7 +15,13 @@ class Pix:
         # qr code
         img = qrcode.make(hash_payment)
 
-        #salvar a imagem como arquivo PNG
-        img.save(f"static/img/qr_code_payment_{bank_payment_id}.png")
+        # Nome do arquivo
+        file_name = f"qr_code_payment_{bank_payment_id}"
 
-        return {"bank_payment_id": bank_payment_id, "qr_code_path": "qr_code_payment_{bank_payment_id}.png"}
+        # Salvar a imagem como arquivo PNG
+        img.save(f"static/img/{file_name}.png")
+
+        return {
+            "bank_payment_id": bank_payment_id,
+            "qr_code_path": file_name,  # Retorna o nome do arquivo sem o caminho completo
+        }
