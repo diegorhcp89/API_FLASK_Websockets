4a9cf496e4fa92055a17d994b6e0f10e74dd756d
diegorhcp89 <diegoferreiradoamaral@gmail.com>
2025-02-14 11:55:43 -0300

    Implementando Confirmação de Pagamento

--- app.py
+++ app.py
@@ -42,7 +42,24 @@ def get_image(file_name):
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def pix_confirmation():
-    return jsonify({"message": "The payment has beeb confirmed"})
+    data = request.get_json()
+
+    # validations
+    if "bank_payment_id" not in data and "value" not in data:
+        return jsonify({"message": "Invalid payment data"}), 400
+
+    # payment ?
+    payment = Payment.query.filter_by(bank_payment_id=data.get("bank_payment_id")).first()
+
+    if not payment or payment.paid:
+        return jsonify({"message": "Payment not found"}), 404
+
+    if data.get("value") != payment.value:
+        return jsonify({"message": "Invalid payment data"}), 400
+    
+    payment.paid = True
+    db.session.commit()
+    return jsonify({"message": "The payment has been confirmed"})
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def payment_pix_page(payment_id):
